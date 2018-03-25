a60fae00d585000428eea064011afd5a47067400
Ricardo Carosa <ricardocarosa@Ricardos-MBP.home>
2018-03-24 20:35:09 -0600

    Attempt 1

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+       teams: ["Yankees", "Bills", "Sabres"]
+    }
+  }
+
+
+
+
   render() {
+
+    let favoriteTeams = this.state.teams.map((element, index) => {
+      return(
+        <h2 key = {index}> {element} </h2> 
+      )
+    })
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className = "App">
+        {favoriteTeams}
       </div>
+    
     );
   }
 }
