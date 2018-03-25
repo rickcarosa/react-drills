707d938d31ee1aaedcb3daf77916a43ee5faf753
Ricardo Carosa <ricardocarosa@Ricardos-MBP.home>
2018-03-25 10:53:02 -0600

    Attempt 1

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,20 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  constructor(){
+    super()
+    this.state = {
+      emptyString: '',
+      teams: ["Yankess", "Bills", "Sabres"]
+    }
+  }
+
+  handleChange(filter){
+    this.setState({
+      emptyString: filter
+    })
+  }
+
+  render(){
+
+    let teamsToDisplay = this.state.teams.filter((element,index) => {
+      return element.includes(this.state.emptyString);
+    }).map((element, index) => {
+      return <h2 key = {index}>{element} </h2>
+    })
+
+    return(
+      <div className = "App">
+
+      <input 
+      onChange = { (event) => this.handleChange(event.target.value)} 
+      type = "text" 
+      />
+      {teamsToDisplay}
       </div>
-    );
+
+    )
   }
 }
 
