1a49991f417f5305df6b76442feff6d60d9e9826
Ricardo Carosa <ricardocarosa@Ricardos-MacBook-Pro.local>
2018-03-26 14:49:30 -0600

    react-drill-4.2

---
+++ app-4/src/Login.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+    constructor(){
+        super()
+        this.State = {
+          username: '',
+          password: ''
+        }
+
+        this.handleLogin = this.handleLogin.bind(this);
+
+      }
+
+      handleUsernameChange(name){
+          this.setState({
+              username: name
+          })
+      }
+
+      handlePasswordChange(pass){
+          this.setState({
+              password: pass
+          })
+      }
+
+      handleLogin(){
+          alert(`Username: ${this.state.username} Password: ${this.state.password}`)
+      }
+
+    render(){
+        return(
+            <div className = "App" >
+            <input onChange = {(event) => this.handleUsernameChange(event.target.value)} type = "text" />
+            <input onChange = {(event) => this.handlePasswordChange(event.target.value)} type = "text" />
+        
+            <button onClick = {this.handleLogin}> Login </button>
+
+            </div>
+        )
+    }
+
+}
+
+export default Login;
