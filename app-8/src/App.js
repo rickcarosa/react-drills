e7cc6482b1d95bc609e3650edbe47109457e4eb7
Ricardo Carosa <ricardocarosa@Ricardos-MBP.home>
2018-04-08 11:08:17 -0600

    react drill 8.2

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,20 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  constructor(){
+    super()
+    this.state = {
+      lukeSkywalker: ''
+    }
+  }
+
+  componentDidMount(){
+    axios.get("https://swapi.co/api/people/1").then( response => {
+      this.setState({
+        lukeSkywalker: response.data
+      })
+    })
+  }
+
+  render(){
+    return(
+      <div className = "App">
+
+      <h1> Name: {this.state.lukeSkywalker.name} </h1>
+      <h2> Birth Year: {this.state.lukeSkywalker.birth_year} </h2>
+      <h3> Height: {this.state.lukeSkywalker.height} </h3>
+      <h4> Eye Color: {this.state.lukeSkywalker.eye_color} </h4>
+ 
       </div>
-    );
+    )
   }
 }
 
